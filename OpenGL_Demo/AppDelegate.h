22fa72309913f8c2eac41e54fa05f0f987d0f0c6
zyqi <449115125@qq.com>
2020-07-03 10:43:56 +0800

    Initial Commit

---
+++ OpenGL_Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL_Demo
+//
+//  Created by dianchou-ios on 2020/7/3.
+//  Copyright © 2020 dianchou-ios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OpenGL_Demo/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  OpenGL_Demo
+//
+//  Created by dianchou-ios on 2020/7/3.
+//  Copyright © 2020 dianchou-ios. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
